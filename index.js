767ebc4114b223736f008a9db9c6618d58f74f72
RIYAZ <smkriyazoutlook@gmail.com>
2023-07-03 09:11:21 -0700

    uploaded successfully

---
+++ index.js
@@ -0,0 +1,61 @@
+// document.getElementsByClassName("btn")[0].addEventListener("click",function(){
+//     document.getElementsByTagName("body")[0].style.backgroundColor="white";
+//     document.getElementsByTagName("h1")[0].style.color="black";
+//    document.getElementsByTagName("a")[0].style.color="black";
+//    document.getElementsByTagName("a")[1].style.color="black";
+//    document.getElementsByTagName("a")[2].style.color="black";
+//    document.getElementsByTagName("h2")[0].style.color="black";
+//    document.getElementsByTagName("h4")[0].style.color="black";
+   
+    
+//     document.getElementsByClassName("btn")[0].innerHTML="NightMode";
+//     if( document.getElementsByClassName("btn")[0].innerHTML="NightMode"){
+//         document.getElementsByClassName("btn")[0].addEventListener("click",function(){
+//             document.getElementsByTagName("body")[0].style.backgroundColor="black"; 
+//             document.getElementsByClassName("btn")[0].innerHTML="LightMode";
+//             document.getElementsByTagName("h1")[0].style.color="white";
+//             document.getElementsByTagName("a")[0].style.color="white";
+//             document.getElementsByTagName("a")[1].style.color="white";
+//             document.getElementsByTagName("a")[2].style.color="white";
+//             document.getElementsByTagName("h2")[0].style.color="white";
+//    document.getElementsByTagName("h4")[0].style.color="white";
+//         })
+   
+       
+//     }
+//     else{
+//         document.getElementsByClassName("btn")[0].addEventListener("click",function(){
+//             document.getElementsByTagName("body")[0].style.backgroundColor="white"; 
+//             document.getElementsByClassName("btn")[0].innerHTML="NightMode"; 
+//         })
+//     }
+// })
+
+
+document.getElementsByClassName("Light")[0].addEventListener("click",function(){
+    document.getElementsByTagName("body")[0].style.backgroundColor="white";
+    document.getElementsByTagName("h1")[0].style.color="black";
+    document.getElementsByTagName("h1")[0].style.color="black";
+        document.getElementsByTagName("a")[0].style.color="black";
+        document.getElementsByTagName("a")[1].style.color="black";
+        document.getElementsByTagName("a")[2].style.color="black";
+        document.getElementsByTagName("h2")[0].style.color="black";
+        document.getElementsByTagName("h4")[0].style.color="black";
+})
+
+document.getElementsByClassName("night")[0].addEventListener("click",function(){
+    document.getElementsByTagName("body")[0].style.backgroundColor="black"; 
+    
+    document.getElementsByTagName("h1")[0].style.color="white";
+    document.getElementsByTagName("a")[0].style.color="white";
+    document.getElementsByTagName("a")[1].style.color="white";
+    document.getElementsByTagName("a")[2].style.color="white";
+    document.getElementsByTagName("h2")[0].style.color="white";
+})
+
+
+
+
+
+
+
